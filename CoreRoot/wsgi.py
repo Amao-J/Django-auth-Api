a089338449db874e8beadd9cb5960df24fe9dfe3
Amao-J <fredjonathan22@yahoo.com>
2024-08-01 00:56:28 +0100

    Update wsgi.py

--- CoreRoot/wsgi.py
+++ CoreRoot/wsgi.py
@@ -1,13 +1,7 @@
-"""
-WSGI config for CoreRoot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
+import sys
 import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 
