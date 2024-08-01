da53a656d4075d6199d73db96cc4f8d65c9af41f
Amao-J <fredjonathan22@yahoo.com>
2024-08-01 01:02:16 +0100

    Update wsgi.py

--- CoreRoot/wsgi.py
+++ CoreRoot/wsgi.py
@@ -2,11 +2,9 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoreRoot.settings')
-
 application = get_wsgi_application()
-
-app = application
