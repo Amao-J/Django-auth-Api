c0c3c4431e5575fae08ec1ad3793e2ac702776ec
Amao-J <fredjonathan22@yahoo.com>
2024-08-01 23:11:13 +0100

    Update settings.py

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -217,5 +217,5 @@ CSRF_TRUSTED_ORIGINS = [
     'https://possible-gar-partially.ngrok-free.app'
 ]
 
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
