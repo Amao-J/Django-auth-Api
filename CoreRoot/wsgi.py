4beb5849dc92b620e9b3279268ca512133412da9
Amao-J <fredjonathan22@yahoo.com>
2024-08-01 12:54:41 +0100

    Update wsgi.py

--- CoreRoot/wsgi.py
+++ CoreRoot/wsgi.py
@@ -1,8 +1,7 @@
-import sys
+
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
 
 from django.core.wsgi import get_wsgi_application
 
