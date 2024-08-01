4eee3c78710569153068b754d0387e7beabe1b12
Amao-J <fredjonathan22@yahoo.com>
2024-08-01 11:24:18 +0100

    Update settings.py

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -155,7 +155,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'user.User'
+AUTH_USER_MODEL = 'core.user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
