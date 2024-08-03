95018d58fad193e525c0a8cb9a5c20b733bd916d
Amao-J <fredjonathan22@yahoo.com>
2024-08-03 18:15:51 +0100

    Update wsgi.py

--- CoreRoot/wsgi.py
+++ CoreRoot/wsgi.py
@@ -5,5 +5,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+project_home = 'Django-auth-Api/CoreRoot'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CoreRoot.settings')
 application = get_wsgi_application()
