24c768555315422bf95a92c1d9fab1c8911921bd
Amao-J <fredjonathan22@yahoo.com>
2024-08-04 12:48:11 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Install dependencies using the full path if necessary
-/usr/local/bin/pip install -r requirements.txt
+pip install -r requirements.txt
 
-# Collect static files using the full path if necessary
-/usr/local/bin/python manage.py collectstatic --noinput
+
+python manage.py collectstatic --noinput
