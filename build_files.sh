34b73405fdebaaa2ca09a63ba4846a5d00b0568c
Amao-J <fredjonathan22@yahoo.com>
2024-08-04 13:46:36 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+echo "Building project packages..."
+python3 -m pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --noinput
+echo "Migrating Database..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
